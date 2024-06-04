49cfb7da33596c6c329885775e90f5df72f3b381
HSDoya <102749850+HSDoya@users.noreply.github.com>
2024-06-04 14:10:25 +0900

    pause button

--- Assets/Script 1/UI/Pause.cs
+++ Assets/Script 1/UI/Pause.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class Pause : MonoBehaviour
 {
     private bool isPaused = false;
+    [SerializeField]
+    private GameObject PauseUI;
 
 
-    // 게임을 멈추는 함수
-    private void PauseGame()
+    private void Awake()
     {
+        PauseUI.SetActive(false);
+    }
+    // 게임을 멈추는 함수
+    void PauseGame()
+    {
+        PauseUI.SetActive(true);
         Time.timeScale = 0;
         isPaused = true;
     }
 
     // 게임을 다시 시작하는 함수
-    private void ResumeGame()
+   void ResumeGame()
     {
+        PauseUI.SetActive(false);
         Time.timeScale = 1;
         isPaused = false;
     }
